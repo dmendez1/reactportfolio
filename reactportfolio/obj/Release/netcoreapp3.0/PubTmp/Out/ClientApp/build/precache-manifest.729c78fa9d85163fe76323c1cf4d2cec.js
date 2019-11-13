64403ee98d6b0ec8762644261265a9d96a91fd9c
David Mendez <davywavy@Davids-MacBook-Pro-3.local>
2019-11-13 14:02:10 -0600

    second commit

---
+++ reactportfolio/obj/Release/netcoreapp3.0/PubTmp/Out/ClientApp/build/precache-manifest.729c78fa9d85163fe76323c1cf4d2cec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b50dfc1c6592b4aaa88b",
+    "url": "/static/js/main.b50dfc1c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b50dfc1c6592b4aaa88b",
+    "url": "/static/css/main.96bd616f.chunk.css"
+  },
+  {
+    "revision": "19bb19ab0992f0539665650f9b280ae3",
+    "url": "/index.html"
+  }
+];
